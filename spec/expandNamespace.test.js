c959f5120de5a9862b37fa6f448b188f35ca901c
Kenji Mukai <kensuka21@gmail.com>
2018-04-27 16:24:15 -0400

    Adding unit test

--- spec/expandNamespace.test.js
+++ spec/expandNamespace.test.js
@@ -1,7 +1,7 @@
 const proxyquire = require( 'proxyquire' );
 
 describe( 'The ./lib/expandNamespaces function', ()=>{
-
+ 
   it( 'should keep paths without namespaces unchanged', ()=>{
 
     const expandNamespaces = proxyquire( '../lib/expandNamespace', {
@@ -11,12 +11,37 @@ describe( 'The ./lib/expandNamespaces function', ()=>{
     const expected = './somePath';
     const actual   = expandNamespaces( expected );
 
-    expect( actual ).to.equal( actual );
+    expect( actual ).to.equal( expected );
 
   } );
 
-  it.skip( 'start writing tests here', ()=>{
+  it( 'should return the relative path of expandNamespace.js', ()=>{
 
+    const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+      './loadNamespaces': ()=>( { namespaces: { expandNamespace: './lib/expandNamespace.js' } } )
+    } );
+
+    const expected = '<expandNamespace>';
+    const actual   = expandNamespaces( expected, __dirname );
+
+    expect( actual ).to.equal( '../lib/expandNamespace.js' );
+  
+  } );
+
+  it( 'should throw an error when the namespace does not exists', ()=>{
+
+    const expandNamespaces = proxyquire( '../lib/expandNamespace', {
+      './loadNamespaces': ()=>( { namespaces: { } } )
+    } );
+
+    const expected = '<expandNamespace>';
+
+    expect( ()=>{
+
+      expandNamespaces( expected, __dirname );
+    
+    } ).to.throw( 'namespace <expandNamespace> is not defined.' );
+  
   } );
 
 } );
